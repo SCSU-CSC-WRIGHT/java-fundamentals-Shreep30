b602ae949cf87ba9eed360594189b6d4198a7156
shreep30 <shreepatel2005@gmail.com>
2025-02-05 17:44:25 -0500

    Lab 1

---
+++ Lab1.java
@@ -0,0 +1,17 @@
+public class Lab1 {
+}
+// Exercise 1
+public class Main {
+    public static void main(String[]args){
+        System.out.println("Hello World!");
+    }
+}
+// Exercise 2
+public class Main {
+    public static void main(string[] args) {
+        int x = 10;
+        string message = "The number is";
+
+        System.out.println(message + " " + x);
+    }
+}
